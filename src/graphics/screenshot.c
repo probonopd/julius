e76832f456b1b5c6788d434a6cf756a1cbb063a9
devnoname120 <devnoname120+gh@gmail.com>
2019-02-02 11:11:28 +0100

    [WIP] Vita port

--- src/graphics/screenshot.c
+++ src/graphics/screenshot.c
@@ -5,6 +5,8 @@
 #include "graphics/screen.h"
 #include "graphics/graphics.h"
 
+#include "platform/vita.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -64,6 +66,7 @@ void graphics_save_screenshot(void)
         return;
     }
     const char *filename = generate_filename();
+    filename = vita_prepend_path(filename);
     FILE *fp = fopen(filename, "wb");
     if (!fp) {
         log_error("Unable to write screenshot to:", filename, 0);
