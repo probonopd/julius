31d292523f5a3f0d27323394d553e6e1d4cdafaa
devnoname120 <devnoname120+gh@gmail.com>
2019-06-25 13:09:57 +0200

    Remove apt-get

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 docker exec appimage /bin/bash -c "cd build
-apt-get update
-apt-get install -y wget libcairo2
 wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
