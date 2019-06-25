204664c721b5ea6edddd6101e8f47158509d89c4
devnoname120 <devnoname120+gh@gmail.com>
2019-06-25 13:11:17 +0200

    Update linuxdeploy

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 docker exec appimage /bin/bash -c "cd build
-wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+# wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-centos6-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
 ./linuxdeploy*.AppImage --appimage-extract-and-run --appdir AppDir/ --output appimage
