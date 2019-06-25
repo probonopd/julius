f2b82e5a4400f5a8d2ee74ad02fe49d37dcfddd2
devnoname120 <devnoname120+gh@gmail.com>
2019-06-25 19:07:07 +0200

    ldconfig debug

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 docker exec appimage /bin/bash -c "cd build
+
+ldconfig -p | grep SDL
+
 # wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-centos6-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
