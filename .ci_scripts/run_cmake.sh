56ec2ebe0ea016685991643a3b9e050133261e0c
devnoname120 <devnoname120+gh@gmail.com>
2019-06-25 12:11:50 +0200

    AppImage: Fix script

--- .ci_scripts/run_cmake.sh
+++ .ci_scripts/run_cmake.sh
@@ -2,7 +2,7 @@
 
 case "$BUILD_TARGET" in
 "appimage")
-    docker exec -e BUILD_TARGET="$BUILD_TARGET" -e SDL_LIB="$SDL_LIB" -e SDL_MIXER_LIB="$SDL_MIXER_LIB" appimage /bin/bash ".ci_scripts/install_sdl.h"
+    docker exec -e BUILD_TARGET="$BUILD_TARGET" -e SDL_LIB="$SDL_LIB" -e SDL_MIXER_LIB="$SDL_MIXER_LIB" appimage /bin/bash ".ci_scripts/install_sdl.sh"
 	docker exec appimage /bin/bash -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr .."
 	;;
 "vita")
