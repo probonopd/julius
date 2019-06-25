da422029dbf01d2cafc6a94bb019ad930c3fe330
devnoname120 <devnoname120+gh@gmail.com>
2019-06-25 23:04:52 +0200

    missing linuxdeploy plugin

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -7,6 +7,10 @@ ldconfig -p | grep SDL
 
 # wget https://github.com/TheAssassin/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-centos6-x86_64.AppImage
+
+# This is supposed to be bundled in the previous AppImage but it's not right now. I manually download it as a workaround
+wget https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-x86_64.AppImage
+
 chmod +x linuxdeploy*.AppImage
 
 ./linuxdeploy*.AppImage --appimage-extract-and-run --appdir AppDir/ --output appimage
